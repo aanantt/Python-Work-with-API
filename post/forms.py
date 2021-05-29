52355027304ce641f4e7f85955ab5765afbdae32
aanantt <anantgithub@gmail.com>
2021-05-30 00:32:55 +0530

    29 may 2021

--- post/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import PostComment
-from django import forms
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = PostComment
-        fields = ('name', 'email', 'body')
